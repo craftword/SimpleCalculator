992bab3ab8175afa458f514d1b7e746af0cb2b93
Abimbola <craftwordltd@gmail.com>
2020-04-02 12:32:12 +0100

    fix equal calculator error

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,10 +18,22 @@ namespace SimpleCalculator
         }
         private void Input(string a)
         {
-            if (txtDisplay.Text == "0")
+            if (txtDisplay.Text == "0" && lblDisplay.Content.ToString() == "")
+            {
+                txtDisplay.Text = a;               
+                lblDisplay.Content = a;
+            }
+            else if(txtDisplay.Text == "0")
+            {
                 txtDisplay.Text = a;
+                lblDisplay.Content += a;
+            }
             else
-                txtDisplay.Text += String.Format("{0:#,##0.##}", a); 
+            {
+                txtDisplay.Text += String.Format("{0:#,##0.##}", a);
+                lblDisplay.Content += String.Format("{0:#,##0.##}", a);
+            }
+               
         }
         private void btn_Number_Click(object sender, RoutedEventArgs e)
         {
@@ -32,10 +44,31 @@ namespace SimpleCalculator
         private void btn_Operator_Click(object sender, RoutedEventArgs e)
         {
             Button btn_Operator = (Button)sender;
-            Operator = btn_Operator.Content.ToString();
-            num1 = decimal.Parse(txtDisplay.Text);
-            //Input(Operator);
-            txtDisplay.Text = "0";
+            if(Operator == "")
+            {
+                Operator = btn_Operator.Content.ToString();
+                if(num1 == 0)
+                    num1 = decimal.Parse(txtDisplay.Text);                
+                lblDisplay.Content += Operator;
+                txtDisplay.Text = "0";
+            }
+            else
+            {
+                
+                try
+                {
+                    num1 = decimal.Parse(BasicMathematicsCalculations(num1, decimal.Parse(txtDisplay.Text)));
+                }
+                catch (Exception exc)
+                {
+                    txtDisplay.Text = "Error!:" + exc;
+                }
+
+                Operator = btn_Operator.Content.ToString();                
+                lblDisplay.Content += Operator;
+                txtDisplay.Text = "0";
+            }
+            
         }
 
         private void btnDot_Click(object sender, RoutedEventArgs e)
@@ -53,6 +86,7 @@ namespace SimpleCalculator
             Operator = "";
             num1 = 0;
             num2 = 0;
+            lblDisplay.Content = "";
         }
 
         private void btnBack_Click(object sender, RoutedEventArgs e)
@@ -62,10 +96,12 @@ namespace SimpleCalculator
                 if (txtDisplay.Text.Length == 1)
                 {
                     txtDisplay.Text = "0";
+                    lblDisplay.Content = txtDisplay.Text;
                 }
                 else if (txtDisplay.Text.Length > 0)
                 {
                     txtDisplay.Text = txtDisplay.Text.Substring(0, txtDisplay.Text.Length - 1);
+                    lblDisplay.Content = txtDisplay.Text;
                 }
             }
         }
@@ -83,24 +119,44 @@ namespace SimpleCalculator
         {
             num2 = decimal.Parse(txtDisplay.Text);
             //////////////////////////////// 
+            try
+            {
+                lblDisplay.Content = BasicMathematicsCalculations(num1, num2);
+            }
+            catch (Exception exc)
+            {
+                txtDisplay.Text = "Error!:" + exc;
+            }
+             txtDisplay.Text = "0";
+             Operator = "";
+             num1 = decimal.Parse(lblDisplay.Content.ToString());
+             num2 = 0;
+
+
+        }
+
+        public string BasicMathematicsCalculations (decimal num1, decimal num2)
+        {
+            string result = "";
             switch (Operator)
             {
                 case "+":
-                    txtDisplay.Text = (num1 + num2).ToString();
+                    result = (num1 + num2).ToString();
                     break;
                 case "-":
-                    txtDisplay.Text = (num1 - num2).ToString();
+                    result = (num1 - num2).ToString();
                     break;
                 case "*":
-                    txtDisplay.Text = (num1 * num2).ToString();
+                    result = (num1 * num2).ToString();
                     break;
                 case "/":
-                    txtDisplay.Text = (num1 / num2).ToString();
-                    break;                
-                case "%":
-                    txtDisplay.Text = (num1 % num2).ToString();
+                    result = (num1 / num2).ToString();
+                    break;
+                case "Mod":
+                    result = (num1 % num2).ToString();
                     break;
             }
+            return result;
         }
     }
 }
