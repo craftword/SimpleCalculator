201a15b9ae1f2fdf6a5decb8fc87ac5507172b4f
Abimbola <craftwordltd@gmail.com>
2020-04-02 14:01:48 +0100

    fix negative number error

---
+++ Helper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleCalculator
+{
+    public class Helper
+    {
+        public static string BasicMathematicsCalculations(decimal num1, decimal num2, string Operator)
+        {
+            string result = "";
+            switch (Operator)
+            {
+                case "+":
+                    result = (num1 + num2).ToString();
+                    break;
+                case "-":
+                    result = (num1 - num2).ToString();
+                    break;
+                case "*":
+                    result = (num1 * num2).ToString();
+                    break;
+                case "/":
+                    result = (num1 / num2).ToString();
+                    break;
+                case "Mod":
+                    result = (num1 % num2).ToString();
+                    break;
+            }
+            return result;
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -30,8 +30,8 @@ namespace SimpleCalculator
             }
             else
             {
-                txtDisplay.Text += String.Format("{0:#,##0.##}", a);
-                lblDisplay.Content += String.Format("{0:#,##0.##}", a);
+                txtDisplay.Text +=  a;
+                lblDisplay.Content += a;
             }
                
         }
@@ -57,11 +57,11 @@ namespace SimpleCalculator
                 
                 try
                 {
-                    num1 = decimal.Parse(BasicMathematicsCalculations(num1, decimal.Parse(txtDisplay.Text)));
+                    num1 = decimal.Parse(Helper.BasicMathematicsCalculations(num1, decimal.Parse(txtDisplay.Text),Operator));
                 }
                 catch (Exception exc)
                 {
-                    txtDisplay.Text = "Error!:" + exc;
+                    txtDisplay.Text = "Error!: Invalid Operation";
                 }
 
                 Operator = btn_Operator.Content.ToString();                
@@ -111,7 +111,11 @@ namespace SimpleCalculator
             if (txtDisplay.Text != "0")
             {
                 if (!txtDisplay.Text.Contains("-"))
+                {
                     txtDisplay.Text = "-" + txtDisplay.Text;
+                    lblDisplay.Content = txtDisplay.Text;
+                }
+                    
             }
         }
 
@@ -121,43 +125,22 @@ namespace SimpleCalculator
             //////////////////////////////// 
             try
             {
-                lblDisplay.Content = BasicMathematicsCalculations(num1, num2);
+                lblDisplay.Content = Helper.BasicMathematicsCalculations(num1, num2, Operator);
+                num1 = decimal.Parse(lblDisplay.Content.ToString());
+                txtDisplay.Text = "0";
+                Operator = "";
+                num2 = 0;
             }
             catch (Exception exc)
             {
-                txtDisplay.Text = "Error!:" + exc;
+                txtDisplay.Text = "Error!: Invalid Operation";
             }
-             txtDisplay.Text = "0";
-             Operator = "";
-             num1 = decimal.Parse(lblDisplay.Content.ToString());
-             num2 = 0;
+             
 
 
         }
 
-        public string BasicMathematicsCalculations (decimal num1, decimal num2)
-        {
-            string result = "";
-            switch (Operator)
-            {
-                case "+":
-                    result = (num1 + num2).ToString();
-                    break;
-                case "-":
-                    result = (num1 - num2).ToString();
-                    break;
-                case "*":
-                    result = (num1 * num2).ToString();
-                    break;
-                case "/":
-                    result = (num1 / num2).ToString();
-                    break;
-                case "Mod":
-                    result = (num1 % num2).ToString();
-                    break;
-            }
-            return result;
-        }
+        
 
         
     }
